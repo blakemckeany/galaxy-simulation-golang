cca0747c7a38a812a351718e8910d362813256b2
blakemckeany <blakemckeany@gmail.com>
2024-05-19 20:53:21 +1200

    Random array of 'particles'

--- hello.go
+++ hello.go
@@ -1,23 +1,27 @@
 package main
 
 import (
-    "log"
-    "runtime"
+	"log"
+	"math/rand"
+	"runtime"
+	"time"
 
-    "github.com/go-gl/gl/v4.1-core/gl" // OR: github.com/go-gl/gl/v2.1/gl
-    "github.com/go-gl/glfw/v3.2/glfw"
-)
+	"fmt"
+	"strings"
 
-import (
-    "strings"
-    "fmt"
+	"github.com/go-gl/gl/v4.1-core/gl" // OR: github.com/go-gl/gl/v2.1/gl
+	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
 const (
-    width  = 500
-    height = 500
+	width  = 720
+	height = 720
 
-    vertexShaderSource = `
+	particleCount = 10
+	particleSize  = 0.02
+
+	// Vertex shader, GLSL
+	vertexShaderSource = `
         #version 410
         in vec3 vp;
         void main() {
@@ -25,7 +29,8 @@ const (
         }
     ` + "\x00"
 
-    fragmentShaderSource = `
+	// Fragment shader, GLSL
+	fragmentShaderSource = `
         #version 410
         out vec4 frag_colour;
         void main() {
@@ -34,127 +39,180 @@ const (
     ` + "\x00"
 )
 
+type particle struct {
+	drawable uint32
+
+	x float32
+	y float32
+}
+
 var (
-    // Slice of vertices
-    triangle = []float32{
-        0, 0.5, 0, // top
-        -0.5, -0.5, 0, // left
-        0.5, -0.5, 0, // right
-    }
+	// Slice of vertices for a square
+
+	square = []float32{
+
+		-particleSize, particleSize, 0, // top
+		-particleSize, -particleSize, 0, // left
+		particleSize, -particleSize, 0, // right
+
+		-particleSize, particleSize, 0, // top
+		particleSize, particleSize, 0, // left
+		particleSize, -particleSize, 0, // right
+	}
 )
 
 func main() {
-    runtime.LockOSThread()
 
-    window := initGlfw()
-    defer glfw.Terminate()
+	rand.Seed(time.Now().UnixNano())
 
-    program := initOpenGL()
+	runtime.LockOSThread()
 
-    vao := makeVao(triangle)
+	window := initGlfw()
+	defer glfw.Terminate()
 
-    for !window.ShouldClose() {
-        // TODO
-        draw(vao, window, program)
-    }
+	program := initOpenGL()
+
+	vao := makeParticles()
+
+	for !window.ShouldClose() {
+		// TODO
+		draw(vao, window, program)
+	}
 }
 
-func draw(vao uint32, window *glfw.Window, prog uint32) {
-    gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-    gl.UseProgram(prog)
+func (p *particle) draw() {
+	gl.BindVertexArray(p.drawable)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(square)/3))
+}
 
-    gl.BindVertexArray(vao)
-    gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle) / 3))
-    
-    glfw.PollEvents()
-    window.SwapBuffers()
+func draw(particles []*particle, window *glfw.Window, prog uint32) {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(prog)
+
+	for _, p := range particles {
+		p.draw()
+	}
+
+	glfw.PollEvents()
+	window.SwapBuffers()
 }
 
 // makeVao initializes and returns a vertex array from the points provided.
 func makeVao(points []float32) uint32 {
-    var vbo uint32
-    gl.GenBuffers(1, &vbo)
-    gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-    gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
-    
-    var vao uint32
-    gl.GenVertexArrays(1, &vao)
-    gl.BindVertexArray(vao)
-    gl.EnableVertexAttribArray(0)
-    gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-    gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
-    
-    return vao
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	return vao
+}
+
+func makeParticles() []*particle {
+	particles := make([]*particle, particleCount)
+	for x := 0; x < particleCount; x++ {
+		// Random X, Random Y between -1 and 1
+
+		xPosition := rand.Float32()*2 - 1
+		fmt.Println(xPosition)
+
+		yPosition := rand.Float32()*2 - 1
+
+		p := newParticle(xPosition, yPosition)
+		particles[x] = p
+	}
+
+	return particles
+}
+
+func newParticle(x, y float32) *particle {
+	points := make([]float32, len(square))
+	copy(points, square)
+
+	// Set the x and y position of the particle
+	for i := 0; i < len(points); i += 3 {
+		points[i] += x
+		points[i+1] += y
+	}
+
+	return &particle{
+		drawable: makeVao(points),
+
+		x: x,
+		y: y,
+	}
 }
 
 func compileShader(source string, shaderType uint32) (uint32, error) {
-    shader := gl.CreateShader(shaderType)
-    
-    csources, free := gl.Strs(source)
-    gl.ShaderSource(shader, 1, csources, nil)
-    free()
-    gl.CompileShader(shader)
-    
-    var status int32
-    gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
-    if status == gl.FALSE {
-        var logLength int32
-        gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
-        
-        log := strings.Repeat("\x00", int(logLength+1))
-        gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
-        
-        return 0, fmt.Errorf("failed to compile %v: %v", source, log)
-    }
-    
-    return shader, nil
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
 }
-
-
 
 // initOpenGL initializes OpenGL and returns an intiialized program.
 func initOpenGL() uint32 {
-    if err := gl.Init(); err != nil {
-            panic(err)
-    }
-    version := gl.GoStr(gl.GetString(gl.VERSION))
-    log.Println("OpenGL version", version)
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
 
-    vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
-    if err != nil {
-        panic(err)
-    }
-    fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
-    if err != nil {
-        panic(err)
-    }
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
 
-    prog := gl.CreateProgram()
-    gl.AttachShader(prog, vertexShader)
-    gl.AttachShader(prog, fragmentShader)
-    gl.LinkProgram(prog)
-    return prog
+	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+	gl.LinkProgram(prog)
+	return prog
 }
 
 // initGlfw initializes glfw and returns a Window to use.
 func initGlfw() *glfw.Window {
-    if err := glfw.Init(); err != nil {
-            panic(err)
-    }
-    
-    glfw.WindowHint(glfw.Resizable, glfw.False)
-    glfw.WindowHint(glfw.ContextVersionMajor, 4) // OR 2
-    glfw.WindowHint(glfw.ContextVersionMinor, 1)
-    glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-    glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
 
-    window, err := glfw.CreateWindow(width, height, "Conway's Game of Life", nil, nil)
-    if err != nil {
-            panic(err)
-    }
-    window.MakeContextCurrent()
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4) // OR 2
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
-    return window
+	window, err := glfw.CreateWindow(width, height, "Conway's Game of Life", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	return window
 }
-
-
